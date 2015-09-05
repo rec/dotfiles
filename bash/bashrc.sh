dd13ba0103321ef17ff4a90c3ed4ec991f912dd3
Tom Ritchford <tom@swirly.com>
2015-09-05 19:05:58 -0400

    Intermediate checkin.

---
+++ bash/bashrc.sh
@@ -0,0 +1,3 @@
+. "$HOME/dotfiles/bash/environment_variables.sh"
+. "$HOME/dotfiles/bash/aliases.sh"
+. "$HOME/dotfiles/bash/interactive.sh"
--- bash/interactive.sh
+++ bash/interactive.sh
@@ -2,8 +2,8 @@
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
 
-. "$HOME/.env"
-. "$HOME/.alias"
+. "$HOME/dotfiles/bash/environment_variables.sh"
+. "$HOME/dotfiles/bash/aliases.sh"
 
 # If not running interactively, don't do anything
 case $- in
