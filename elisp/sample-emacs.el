7abeaa55ea2920a419350ab23c166949518eb861
Tom Ritchford <tom@swirly.com>
2015-09-05 22:22:49 -0400

    Sample emacs.el

---
+++ elisp/sample-emacs.el
@@ -0,0 +1,4 @@
+(setq load-path (cons (expand-file-name "~/dotfiles/elisp") load-path))
+(setq load-path (cons (expand-file-name "~/dotfiles/elisp/libraries") load-path))
+
+(load-library "startup")
