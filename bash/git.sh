0dde9d0f667498975bfa54fff1c6b35bdc57252e
Tom Ritchford <tom@swirly.com>
2017-04-01 20:59:02 +0200

    Better gfresh and gdelete.

--- bash/git.sh
+++ bash/git.sh
@@ -11,30 +11,34 @@ function gcom() {
 
 # Commit everything with a message and push it.
 function gcomp() {
-    git commit -am "$*" && git push
+    git commit -am "$*" && \
+        git push
 }
 
 # Check out a copy of the current branch under a new name and push it to your
 # origin directory.
 function gnew() {
-    git checkout -b $1 && git push --set-upstream origin $1
+    git checkout -b $1 && \
+        git push --set-upstream origin $1
 }
 
 # Commit everything, then revert to the previous commit.
 # Think of it as a safer reset.
 
 function greset() {
-    git commit -am "reset" && git reset --hard HEAD^
+    git commit -am "reset" && \
+        git reset --hard HEAD^
 }
 
 # Back up the current branch.
 function gback() {
     BRANCH=`git symbolic-ref --short HEAD`
     BACKUP=b-$BRANCH
-    gdelete-f $BACKUP
-    git checkout $BRANCH
-    gnew $BACKUP
-    git checkout $BRANCH
+
+    gdelete-f $BACKUP&& \
+        git checkout $BRANCH && \
+        gnew $BACKUP && \
+        git checkout $BRANCH
 }
 
 # Amend the previous change to include all the changes you have currently.
@@ -47,56 +51,67 @@ function gamend() {
 # and force push. gcap is "git commit, amend, push"
 # Slightly dangerous, don't use this on master.
 function gcap() {
-    git commit --amend -a --no-edit && git push -f
+    git commit --amend -a --no-edit && \
+        git push -f
+}
+
+function gfresh-f() {
+    BASE=`/development/dotfiles/python/base_branch.py`
+
+    git checkout -b $1 && \
+        git fetch upstream && \
+        git reset --hard upstream/$BASE && \
+        git push --set-upstream origin $1
 }
 
 # Check out a fresh copy of master under a new name and push it to your origin
 # directory.
 function gfresh() {
-    git checkout `/development/dotfiles/python/base_branch` && git pull && gnew $1
+    if git diff-index --quiet HEAD -- ; then
+        gfresh-f "$1"
+    else
+        echo "ERROR: Changes in your workspace would be overwritten."
+     fi
 }
 
 # Delete branches that have been merged to master.
 function gdelete() {
     BRANCH=`git symbolic-ref --short HEAD`
+
     for i in $@
     do
-        git checkout `/development/dotfiles/python/base_branch`
-        git branch -d $i && git push --delete origin $i
+        git checkout `/development/dotfiles/python/base_branch.py` && \
+            git branch -d $i && \
+            git push --delete origin $i
     done
+
     git checkout $BRANCH
 }
 
 # Delete branches that might not have been merged to master.
 function gdelete-f() {
     BRANCH=`git symbolic-ref --short HEAD`
+
     for i in $@
     do
-        git checkout `/development/dotfiles/python/base_branch`
-        git branch -D $i
-        git push --delete origin $i
+        git checkout `/development/dotfiles/python/base_branch.py` && \
+            git branch -D $i && \
+            git push --delete origin $i
     done
+
     git checkout $BRANCH
 }
 
 # Move an existing branch to a new name.
 function gmove() {
     BRANCH=`git symbolic-ref --short HEAD`
+
     git checkout $1 && \
         git pull && \
         git branch -m $1 $2 && \
         git push origin :$1 && \
         git push --set-upstream origin $2
-    git checkout $BRANCH
-}
 
-# Merge a branch onto `/development/dotfiles/python/base_branch`.
-function gmerge() {
-    BRANCH=`git symbolic-ref --short HEAD`
-    git checkout `/development/dotfiles/python/base_branch` && \
-        git merge --ff-only $1 && \
-        git push && \
-        gdelete $1
     git checkout $BRANCH
 }
 
---
+++ python/base_branch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os, subprocess, sys
+
+def get_usused_branch(branches):
+    def get():
+        subout = subprocess.check_output(('git', 'branch'))
+        for line in subout.splitlines():
+            line = line.decode()
+            while line.startswith('*'):
+                line = line[1:]
+            yield line.strip()
+
+    existing = set(get())
+    unused = sorted(existing - set(branches))
+    return unused and unused[0]
+
+
+
+if __name__ == '__main__':
+    branch = get_usused_branch(sys.argv[1:])
+    if not branch:
+        print('ERROR: Can\'t delete all branches', file=sys.stderr)
+        sys.exit(-1)
+
+    print(branch)
