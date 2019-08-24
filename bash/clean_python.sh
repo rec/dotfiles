6d62eb7bc2f46770757b0352ac110bfc0f0b6920
Tom Ritchford <tom@swirly.com>
2019-08-24 14:40:38 +0200

    New bash/clean_python.sh

---
+++ bash/clean_python.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PWD=`pwd`
+NAME=`basename $PWD`
+
+source ~/.bashrc
+
+echo "penv $NAME"
+penv $NAME
+
+set -Eeuxo pipefail
+
+if [ $@ ] ; then
+    NAME=$@
+else
+    NAME="$NAME test"
+fi
+
+# find . -name \*.py | xargs /code/env/black/bin/black -l 79 -S
+# mypy $NAME
+flake8 $NAME test
+pytest test
