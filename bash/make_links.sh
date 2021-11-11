14dc694c4ce15beddaa95ec501f4cbdf70d3bfcd
Tom Ritchford <tom@swirly.com>
2021-11-11 20:02:12 +0100

    Add bash/make_links.sh

---
+++ bash/make_links.sh
@@ -0,0 +1,5 @@
+ln -s /code/dotfiles/dotfiles/.bash_profile ~/.bash_profile
+ln -s /code/dotfiles/dotfiles/.bashrc ~/.bashrc
+ln -s /code/dotfiles/dotfiles/.emacs ~/.emacs
+ln -s /code/dotfiles/dotfiles/.emacs.d ~/.emacs.d
+ln -s /code/dotfiles/dotfiles/.gitconfig ~/.gitconfig
