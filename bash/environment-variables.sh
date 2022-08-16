85837054adc79e2fd83378fe331a4f0932c880af
Tom Ritchford <tom@swirly.com>
2022-08-16 18:05:45 +0200

    Modify bash/environment-variables.sh

--- bash/environment-variables.sh
+++ bash/environment-variables.sh
@@ -22,24 +22,21 @@ export SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 export VIRTUALENV_CONFIG_FILE=/code/dotfiles/virtualenv.ini
 
-# export FLASK_APP=/code/microblog/microblog.py
-# export LDFLAGS="-L/usr/local/opt/qt/lib"
-# export CPPFLAGS="-I/usr/local/opt/qt/include"
-# export PYTHONSTARTUP=~/bin/python_startup.py
-
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
 }
 
 export PS1="\[\033[32m\]\$(parse_git_branch)\[\033[00m\]\u@\h:\w$ "
 
-PV=/Library/Frameworks/Python.framework/Versions
-
 export PATH=\
 /Users/tom/bin:\
 /code/gitz:\
 /code/dotfiles/bash/:\
 /code/engora-search/scripts:\
+
+if [[ "$(/bin/hostname)" == "bantam.local" ]]; then
+    PV=/Library/Frameworks/Python.framework/Versions
+    PATH=\
 $PV/2.7/bin:\
 $PV/3.6/bin:\
 $PV/3.7/bin:\
@@ -48,11 +45,26 @@ $PV/3.9/bin:\
 $PV/3.10/bin:\
 /Applications/Postgres.app/Contents/Versions/13/bin:\
 /usr/local/opt/openssl/bin:\
-/usr/local/bin:\
+/usr/local/bin
+
+fi
+
+if [[ "$(/bin/hostname)" == "bolt.local" ]]; then
+    PATH=$PATH:/opt/homebrew/bin
+fi
+
+PATH=$PATH:\
 /usr/bin:\
 /bin:\
 /usr/sbin:\
-/sbin:\
+/sbin
+
+export PATH
+
+# export FLASK_APP=/code/microblog/microblog.py
+# export LDFLAGS="-L/usr/local/opt/qt/lib"
+# export CPPFLAGS="-I/usr/local/opt/qt/include"
+# export PYTHONSTARTUP=~/bin/python_startup.py
 
 # /usr/local/opt/qt/bin
 # /opt/X11/bin:\
@@ -60,5 +72,3 @@ $PV/3.10/bin:\
 # /Applications/Aquamacs.app/Contents/MacOS/bin/:\
 # /opt/local/bin:\
 # /Users/tom/.virtualenvs/util/bin:\
-
-export PATH
