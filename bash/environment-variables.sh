02adaf600dd8646a8551c108c41bc0de70ca70b0
Tom Ritchford <tom@swirly.com>
2022-08-17 12:19:45 +0200

    Major cleanup

--- bash/environment-variables.sh
+++ bash/environment-variables.sh
@@ -1,24 +1,11 @@
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
-export CHOPIN=192.168.178.108
-
 export DIRENV_LOG_FORMAT=
 
 export EDITOR=emacsclient
 export ENGORA_BACKGROUND_MUSIC=false
 
-export GIT_USER=rec
-
-export HISTFILESIZE=100000
-export HISTSIZE=10000
-
 export LANG=en_US.UTF-8
-
-export PENV_PYTHON=python3.8
-export PENV_ROOT=~/.virtualenvs
-export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig"
-
-export SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 export VIRTUALENV_CONFIG_FILE=/code/dotfiles/virtualenv.ini
 
@@ -32,11 +19,11 @@ export PATH=\
 /Users/tom/bin:\
 /code/gitz:\
 /code/dotfiles/bash/:\
-/code/engora-search/scripts:\
+/code/engora-search/scripts
 
 if [[ "$(/bin/hostname)" == "bantam.local" ]]; then
     PV=/Library/Frameworks/Python.framework/Versions
-    PATH=\
+    export PATH=$PATH:\
 $PV/2.7/bin:\
 $PV/3.6/bin:\
 $PV/3.7/bin:\
@@ -53,22 +40,10 @@ if [[ "$(/bin/hostname)" == "bolt.local" ]]; then
     PATH=$PATH:/opt/homebrew/bin
 fi
 
-PATH=$PATH:\
+export PATH=$PATH:\
 /usr/bin:\
 /bin:\
 /usr/sbin:\
 /sbin
 
-export PATH
-
-# export FLASK_APP=/code/microblog/microblog.py
-# export LDFLAGS="-L/usr/local/opt/qt/lib"
-# export CPPFLAGS="-I/usr/local/opt/qt/include"
 # export PYTHONSTARTUP=~/bin/python_startup.py
-
-# /usr/local/opt/qt/bin
-# /opt/X11/bin:\
-# /usr/local/sbin:\
-# /Applications/Aquamacs.app/Contents/MacOS/bin/:\
-# /opt/local/bin:\
-# /Users/tom/.virtualenvs/util/bin:\
--- dotfiles/.bashrc
+++ dotfiles/.bashrc
@@ -2,18 +2,16 @@
 
 export CODE_ROOT=/code
 
-. $CODE_ROOT/dotfiles/bash/environment-variables.sh
+SECRETS=~/.secrets.sh
+
+if [ -f $SECRETS ]; then
+    . $SECRETS
+fi
+
 . $CODE_ROOT/dotfiles/bash/aliases.sh
+. $CODE_ROOT/dotfiles/bash/environment-variables.sh
 . $CODE_ROOT/dotfiles/bash/functions.sh
 . $CODE_ROOT/dotfiles/bash/interactive.sh
-. ~/.secrets.sh
 . $CODE_ROOT/pppp/pppp.sh
 
-# . /usr/local/opt/autoenv/activate.sh
-
-# Prevent the activate script from killing $PATH
-# TODO: is this necessary now?
-_OLD_VIRTUAL_PATH=
-
 eval "$(direnv hook bash)"
-# echo 'END: .bashrc'
