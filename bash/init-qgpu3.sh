20bb1869d6e3441b0a95c947ea3e8282c3c99907
Tom Ritchford <tom@swirly.com>
2025-03-13 12:40:05 +0000

    bash/init-qgpu3.sh

--- bash/init-qgpu3.sh
+++ bash/init-qgpu3.sh
@@ -1,3 +1,3 @@
-export PATH="$PATH:$CODE_ROOT/gitz:~/git/torch-build:/opt/conda/bin"
+export PATH="$PATH:$CODE_ROOT/gitz:~/git/torch-build"
 source ~/git/torch-build/torch-common.sh
 export COLORTERM=truecolor
