bb04ae03e91c34d403e35bbe3f0eee997dfc933c
Tom Ritchford <tom@swirly.com>
2025-06-12 13:59:19 +0200

    Several changes
    Modify bash/environment-variables.sh
    Add bash/copy-environment.sh

--- bash/environment-variables.sh
+++ bash/environment-variables.sh
@@ -17,4 +17,23 @@ parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
 }
 
-export PS1="\[\033[32m\]\$(parse_git_branch)\[\033[36m\]\D{%m/%d}-\t \[\033[00m\]\u@\h:\w$ "
+if [ tput setaf 128 2> /dev/null ] ; then
+    branch_c="\[$(tput setaf 226)\]"
+    time_c="\[$(tput setaf 226)\]"
+    user_c="\[$(tput setaf 226)\]"
+    dir_c="\[$(tput setaf 226)\]"
+else
+    branch_c="\[\033[32m\]"
+    time_c="\[\033[36m\]"
+    user_c="\[\033[00m\]"
+    dir_c="\[\033[00m\]"
+fi
+
+export PS1="\
+$branch_c\$(parse_git_branch)\
+$time_c\D{%m/%d}-\t\
+ $user_c\u\
+$dir_c\h:\
+$dir_c\w$ "
+
+# export PS1="\[\033[32m\]\$(parse_git_branch)\[\033[36m\]\D{%m/%d}-\t \[\033[00m\]\u@\h:\w$ "
