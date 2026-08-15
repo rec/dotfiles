8ac151e4661461844b38c0452d41983fe7b494b0
Tom Ritchford <tom@swirly.com>
2026-08-15 15:27:22 +0200

    Add rotp bash function

--- bash/_functions.sh
+++ bash/_functions.sh
@@ -1,3 +1,25 @@
 build-stubs() {
     (cd build && cmake --build . --target torch_python_stubs)
 }
+
+rotp() {
+    local project
+    local root
+
+    for project in lyte recs showco twitcho; do
+        root="$HOME/code/$project"
+        case "$PWD/" in
+            "$root"/*)
+                case "$project" in
+                    lyte) cd "$HOME/code/recs" ;;
+                    recs) cd "$HOME/code/showco" ;;
+                    showco) cd "$HOME/code/twitcho" ;;
+                    twitcho) cd "$HOME/code/lyte" ;;
+                esac
+                return
+                ;;
+        esac
+    done
+
+    cd "$HOME/code/lyte"
+}
